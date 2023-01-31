77106e8c3adb80f9342900cac0ee073ac362126a
Sayan Biswas <sayanbiswas2468@gmail.com>
2023-01-31 18:21:15 +0530

    first

---
+++ wp-login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Log In &lsaquo; Travel &#8212; WordPress</title>
+	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link rel="stylesheet" id="dashicons-css" href="https://sayanbiswas0408.github.io/Travel-Website/wp-includes/css/dashicons.min.css?ver=6.1.1" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://sayanbiswas0408.github.io/Travel-Website/wp-includes/css/buttons.min.css?ver=6.1.1" media="all">
+<link rel="stylesheet" id="forms-css" href="https://sayanbiswas0408.github.io/Travel-Website/wp-admin/css/forms.min.css?ver=6.1.1" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://sayanbiswas0408.github.io/Travel-Website/wp-admin/css/l10n.min.css?ver=6.1.1" media="all">
+<link rel="stylesheet" id="login-css" href="https://sayanbiswas0408.github.io/Travel-Website/wp-admin/css/login.min.css?ver=6.1.1" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="http://localhost/TravelWebsite/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
+									<input type="hidden" name="redirect_to" value="http://localhost/TravelWebsite/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+				<a href="https://sayanbiswas0408.github.io/Travel-Website/wp-login.php?action=lostpassword">Lost your password?</a>			</p>
+					<script type="text/javascript">function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
+				<p id="backtoblog">
+			<a href="https://sayanbiswas0408.github.io/Travel-Website/">&larr; Go to Travel</a>		</p>
+			</div>
+			<script src="https://sayanbiswas0408.github.io/Travel-Website/wp-includes/js/jquery/jquery.min.js?ver=3.6.1" id="jquery-core-js"></script>
+<script src="https://sayanbiswas0408.github.io/Travel-Website/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script id="zxcvbn-async-js-extra">var _zxcvbnSettings = {"src":"https:\/\/sayanbiswas0408.github.io\/Travel-Website\/wp-includes\/js\/zxcvbn.min.js"};</script>
+<script src="https://sayanbiswas0408.github.io/Travel-Website/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="https://sayanbiswas0408.github.io/Travel-Website/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
+<script src="https://sayanbiswas0408.github.io/Travel-Website/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script src="https://sayanbiswas0408.github.io/Travel-Website/wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
+<script src="https://sayanbiswas0408.github.io/Travel-Website/wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
+<script id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+</script>
+<script id="password-strength-meter-js-extra">var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};</script>
+<script src="https://sayanbiswas0408.github.io/Travel-Website/wp-admin/js/password-strength-meter.min.js?ver=6.1.1" id="password-strength-meter-js"></script>
+<script src="https://sayanbiswas0408.github.io/Travel-Website/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/TravelWebsite\/wp-admin\/admin-ajax.php"}};</script>
+<script src="https://sayanbiswas0408.github.io/Travel-Website/wp-includes/js/wp-util.min.js?ver=6.1.1" id="wp-util-js"></script>
+<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"1d83ca1f2d"};</script>
+<script src="https://sayanbiswas0408.github.io/Travel-Website/wp-admin/js/user-profile.min.js?ver=6.1.1" id="user-profile-js"></script>
+			<script>/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);</script>
+				<div class="clear"></div>
+	</body>
+	</html>
